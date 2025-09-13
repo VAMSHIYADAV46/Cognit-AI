9d7900e82cdfcf117545c7c6babb415543a23064
Mekala Vamshi Yadav <nanidx46@gmail.com>
2025-09-13 20:56:39 +0530

    Database connection Implementation

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 
 dotenv.config();
@@ -15,9 +16,20 @@ app.use(cors())
 
 app.listen(port,()=>{
   console.log(`Server is listening on port : ${port}`)
+  ConnectDB()
 })
 
-app.get("/",(req,res)=>{
-  res.send("hello")
-})
+
+const ConnectDB = async () => {
+  
+  try{
+    await mongoose.connect(process.env.MONGODB_URL)
+    .then(console.log(`Database Connected Successfully`))
+  }
+  catch(err){
+    console.log(`Error While Connecting to database : ${err}`)
+  }
+}
+
+
 
