fb7b4b7037d2012fd297194ad3fbd72a335d0624
Mekala Vamshi Yadav <nanidx46@gmail.com>
2025-09-15 23:47:35 +0530

    Context API Implementation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css'
 import  ChatWindow from "./components/ChatWindow.jsx"
 import  Sidebar from "./components/Sidebar.jsx"
+import MyContext from './MyContext.jsx'
 
 function App() {
+  const providerValues = {};
 
 
   return (
     <>
-    <div className='main'>
-
-      <ChatWindow></ChatWindow>
-      <Sidebar></Sidebar>
-
-
+    <div className='app'>
+      <MyContext.Provider value={providerValues}>
+        <ChatWindow></ChatWindow>
+        <Sidebar></Sidebar>
+      </MyContext.Provider>
     </div>
     </>
   )
