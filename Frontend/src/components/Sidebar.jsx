297b573c384c6ace86625ab2080838c1bc95ab87
Mekala Vamshi Yadav <nanidx46@gmail.com>
2025-09-17 11:01:06 +0530

    Sidebar component skeleton implementation

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -2,9 +2,25 @@ import "../styles/Sidebar.css"
 
 function Sidebar(){
     return(
-        <div>
-           <h1 className="Sidebar">Sidebar</h1>
-        </div>
+        <section>
+
+            <button>
+                <img src="./src//assets/black brain logo.svg"></img>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            
+
+            <ul>
+                <li>sample 1</li>
+                <li>sample 2</li>
+                <li>sample 3</li>
+            </ul>
+
+            <div className="sign in">
+                <p>CognitAI  &trade;</p>
+            </div>
+
+        </section>
     )
 }
 
