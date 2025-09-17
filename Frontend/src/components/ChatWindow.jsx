055955428302e7b721d12faef1c1a2847d314d69
Mekala Vamshi Yadav <nanidx46@gmail.com>
2025-09-17 11:40:00 +0530

    ChatWindow component skeleton implementation

--- Frontend/src/components/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow.jsx
@@ -1,10 +1,28 @@
 import "../styles/ChatWindow.css"
+import Chat from "../components/Chat.jsx"
 
 
 function ChatWindow(){
     return(
-        <div>
-            <h1>ChatWindow</h1>
+        <div className="chatWindow">
+            <div className="navbar">
+                <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+            
+            <Chat></Chat>
+            
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything"></input>
+                    <div id="submit" ><i className="fa-solid fa-paper-plane"></i></div>
+                </div>
+                <p className="info">
+                    CognitAI can make mistakes. Check important info.
+                </p>
+            </div>
         </div>
     )
 }
