bf95564e0fd1f84fc8d792df61800723c2e5db80
Mekala Vamshi Yadav <nanidx46@gmail.com>
2025-09-17 20:26:46 +0530

    reply implementation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
 import  ChatWindow from "./components/ChatWindow.jsx"
 import  Sidebar from "./components/Sidebar.jsx"
-import MyContext from './MyContext.jsx'
+import  MyContext  from "./MyContext.jsx";
+
+
+import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const providerValues = {};
+
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv4());
+
+
+  const providerValues = {
+     prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId
+  };
 
 
   return (
--- Frontend/src/components/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow.jsx
@@ -1,9 +1,41 @@
 import "../styles/ChatWindow.css"
 import Chat from "./Chat";
 
+import  MyContext  from "../MyContext.jsx";
+import { useContext, useState, useEffect } from "react";
+
+
 
 
 function ChatWindow(){
+
+     const {prompt, setPrompt, reply, setReply, currThreadId, setPrevChats, setNewChat} = useContext(MyContext);
+
+    const getReply = async () => {
+
+        console.log("message ", prompt, " threadId ", currThreadId);
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try {
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+    
     return(
         <div className="chatWindow">
             <div className="navbar">
@@ -17,8 +49,11 @@ function ChatWindow(){
             
             <div className="chatInput">
                 <div className="inputBox">
-                    <input placeholder="Ask anything"></input>
-                    <div id="submit" ><i className="fa-solid fa-paper-plane"></i></div>
+                    <input placeholder="Ask anything" 
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    onKeyDown={(e) => e.key === 'Enter'? getReply() : ''} ></input>
+                    <div id="submit" onClick={getReply}><i className="fa-solid fa-paper-plane"></i></div>
                 </div>
                 <p className="info">
                     CognitAI can make mistakes. Check important info.
